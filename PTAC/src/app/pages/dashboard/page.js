7ca503ca348f3c1f85a713350ec2b1694831e0fa
Ana Flores <anacarlafloressilva@gmail.com>
2023-09-15 18:58:57 -0400

    alterando o page.js (dashboard)

--- PTAC/src/app/pages/dashboard/page.js
+++ PTAC/src/app/pages/dashboard/page.js
@@ -4,10 +4,15 @@ import Listar from "@/app/componentes/listar";
 
 export default async function Dashboard() {
    
+    const users = getUsers()
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div> 
+            <Suspense fallback={<p> carregando pagina...</p>}>
+                <Listar users={users}/>
+                <button><a href="/pages/alterar">Alterar</a></button>
+                <button><a href="/pages/registrar">Registrar</a></button>
+            </Suspense>
             
         </div>
     );
-};
+};            
